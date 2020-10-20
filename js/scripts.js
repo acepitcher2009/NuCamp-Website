ccbcf3f338f5d1da7c435a34a148a8f1905ae67a
acepitcher2009 <69870798+acepitcher2009@users.noreply.github.com>
2020-10-20 11:24:37 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,9 @@ $(function () {
         }
     });
 });
+$("#reserveButton").click(function () {
+    $("#reserveModal").modal("show");
+})
+$("#loginButton").click(function () {
+    $("#loginModal").modal("show");
+})
